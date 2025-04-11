7fce2780116b3a923340cedd910035e59c766eb5
lucasbezerrademacedo <lucasbezerrademacedo@192.168.108.133>
2025-04-11 16:42:37 -0300

    Segundo commit.

--- src/controller/MenorValorDoVetor.java
+++ src/controller/MenorValorDoVetor.java
@@ -5,14 +5,14 @@ public class MenorValorDoVetor {
 	public MenorValorDoVetor() {
           super();
 	}
-	public int menorvalordovet(int[] vetor, int tamanho, int ultimap) {
-    	if (tamanho < 0) {
-    		return ultimap; // Condicao de parada quando o tamanhoi for menor que a posicao inicial.
+	public int menorvalordovet(int[] vetor, int indice, int menorAtual) {
+    	if (indice < 0) {
+    		return menorAtual; 
     	}
-    	if (vetor [tamanho] < ultimap) { 
-    		ultimap = vetor [tamanho]; // Se a ultima posicao do vetor for menor que o menor valor anterior, o menor valor será substituído por esse novom menor. 
+    	if (vetor [indice] < menorAtual) { 
+    		menorAtual = vetor [indice];  
     	}
-    	tamanho--; // Decrementa o tamanho.
-    	return menorvalordovet(vetor, tamanho, ultimap); //Chama a funcao novamente com os novos parametros.
+    	indice--; 
+    	return menorvalordovet(vetor, indice, menorAtual); 
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -7,15 +7,15 @@ import controller.MenorValorDoVetor;
 public class Principal {
 
 	public static void main(String[] args) {
-		MenorValorDoVetor menorvalordovetor = new MenorValorDoVetor(); // Instancia da classe controller.
-		int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Qual o tamanho do vetor?")); // Definicao do tamanho do vetor.
-		int i; // Contador.
-		int vetor [] = new int [tamanho]; //Criacao do vetor.
-		for (i = 0; i < tamanho; i ++) {
-		vetor [i] = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor da posiçao " + i + " do vetor.")); // Coleta dos valores do vetor.
+		MenorValorDoVetor menorvalordovetor = new MenorValorDoVetor(); 
+		int indice = Integer.parseInt(JOptionPane.showInputDialog("Qual o tamanho do vetor?")); 
+		int i; 
+		int vetor [] = new int [indice]; 
+		for (i = 0; i < indice; i ++) {
+		vetor [i] = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor da posiçao " + i + " do vetor."));
 		}
-		int ultimap = vetor[tamanho - 1]; // Menor valor inicial.
-		int resultado = menorvalordovetor.menorvalordovet(vetor, tamanho-1, ultimap); // Chamada da funcao recursiva para obter o menor valor.
-		JOptionPane.showMessageDialog(null, "A menor posicao do vetor é " + resultado);	// Resultado.	
+		int menorAtual = vetor[indice - 1]; 
+		int resultado = menorvalordovetor.menorvalordovet(vetor, indice-1, menorAtual); 
+		JOptionPane.showMessageDialog(null, "O menor valor contido no vetor é " + resultado);	
 	}
 }
